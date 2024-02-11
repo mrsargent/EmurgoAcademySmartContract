a1b19b011a1936eb9b7dcf27a3e2c8255ad988a0
mrsargent <sargent11235@gmail.com>
2024-02-11 17:31:22 +0000

    updated from sat 2-10

--- V2_110/DemeterAndDocker/EA_NativeScripting/4-MetaData/metadataTx.sh
+++ V2_110/DemeterAndDocker/EA_NativeScripting/4-MetaData/metadataTx.sh
@@ -3,15 +3,15 @@ cardano-cli query protocol-parameters --testnet-magic 2 --out-file protocol.para
 cardano-cli transaction build \
   --babbage-era \
   --testnet-magic 2 \
-  --tx-in "4b6bda5d37598571a89dc876885c5a973bb50e405a50a8545c6f65d395176739#2" \
-  --change-address $nami \
-  --metadata-json-file myData.json \
+  --tx-in "518ced66d03c090eac406b39b6d765d5b223d23d0b0856be9bee73a5d1f878b0#0" \
+  --change-address "addr_test1qqd2cd7rnup5zzylt34na9srfswhe6pvqk6c03zqs6ulklkm8f39nn3f9pdmr0yxeakqnnj90drjjx82vrnpjpw79lsq2caqqf" \
+  --metadata-json-file testFile.json \
   --protocol-params-file protocol.params \
   --out-file metadataTx.unsigned
 
 cardano-cli transaction sign \
   --tx-body-file metadataTx.unsigned \
-  --signing-key-file ../../Wallets/Adr01.skey \
+  --signing-key-file ../1-SimplePayment/batch107.skey \
   --testnet-magic 2 \
   --out-file metadataTx.signed
 
