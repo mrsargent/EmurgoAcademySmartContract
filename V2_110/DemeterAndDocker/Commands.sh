1699247c43026ff64fc784f606db7c2f727ca7b8
mrsargent <sargent11235@gmail.com>
2024-02-24 21:40:51 +0000

    deletes commands.sh

--- V2_110/DemeterAndDocker/Commands.sh
+++
@@ -1,7 +0,0 @@
-export cutxo="cardano-cli query utxo --address $var $network"
-
-export PREVIEW="--testnet-magic 2"
-
-export AdrBatch="addr_test1qqd2cd7rnup5zzylt34na9srfswhe6pvqk6c03zqs6ulklkm8f39nn3f9pdmr0yxeakqnnj90drjjx82vrnpjpw79lsq2caqqf"
-
-echo "Commands loaded"
